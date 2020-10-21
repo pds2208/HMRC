32e50c56c9442c0f81a757e0446c031e863f6bd1
Paul Soule <22938608+pds2208@users.noreply.github.com>
2020-10-21 21:08:01 +0100

    tart up

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,32 +6,21 @@ object Main { // could do this  extends App {
   val ORANGE_COST = .25
   val Apple: Fruit = Fruit(APPLE_COST)
   val Orange: Fruit = Fruit(ORANGE_COST)
+  val cart: List[Fruit] = Orange :: Apple :: Orange :: Orange :: Apple :: Apple :: Orange :: Orange :: Orange :: Apple :: Apple :: Nil
 
   def main(args: Array[String]): Unit = {
-    val cart: List[Fruit] = Orange :: Apple :: Orange :: Orange :: Apple :: Apple :: Orange :: Orange :: Orange :: Apple :: Apple :: Nil
-
     println(s"Total: ${calculateCart(cart)}")
     println(s"Total special: ${calculateSpecial(cart)}")
-
   }
 
-  def calculateCart(basket: List[Fruit]): BigDecimal = {
-    basket match {
-      case x :: tail => x.price + calculateCart(tail)
-      case Nil       => 0
-    }
-  }
+  def calculateCart(basket: List[Fruit]): BigDecimal = basket.map(_.price).sum
 
   def calculateSpecial(xs: List[Fruit]): BigDecimal = {
     val (oq, or) = xs.count(a => a.price == ORANGE_COST) /% 3
     val (aq, ar) = xs.count(a => a.price == APPLE_COST) /% 2
-
-    val total_oranges = oq * (ORANGE_COST * 2) + or * ORANGE_COST
-    val total_apples = aq * APPLE_COST + ar * APPLE_COST
-
-    total_apples + total_oranges
-
+    (oq * (ORANGE_COST * 2) + or * ORANGE_COST) + (aq * APPLE_COST + ar * APPLE_COST)
   }
 
   case class Fruit(price: BigDecimal)
+
 }
